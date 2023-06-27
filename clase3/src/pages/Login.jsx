0b749decc9294622ae0e8185518a2e19f1c5c071
Pedro Wood <pedro.wood@truenorth.co>
2023-06-27 17:14:42 -0300

    clase 3

---
+++ clase3/src/pages/Login.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import {
+  MDBInput,
+  MDBCol,
+  MDBRow,
+  MDBCheckbox,
+  MDBBtn
+} from 'mdb-react-ui-kit';
+
+
+export default function App() {
+  const [userName, setUserName ] = useState(false)
+
+  const loginSubmit = (event) => {
+    event.preventDefault();
+    if (userName == 'admin' && password == 'password123!') {
+      
+    }
+  }
+
+  return (
+    user ?
+      <p>logged in</p>
+      :
+    <form onSubmit={loginSubmit}>
+      <MDBInput className='mb-4' type='email' id='email' label='Email address' onChange={e => setUserName(e.target.value)} />
+      <MDBInput className='mb-4' type='password' id='password' label='Password' />
+
+      <MDBRow className='mb-4'>
+        <MDBCol className='d-flex justify-content-center'>
+          <MDBCheckbox id='form1Example3' label='Remember me' defaultChecked />
+        </MDBCol>
+        <MDBCol>
+          <a href='#!'>Forgot password?</a>
+        </MDBCol>
+      </MDBRow>
+
+      <MDBBtn type='submit' block>
+        Sign in
+      </MDBBtn>
+    </form>
+  );
+}
